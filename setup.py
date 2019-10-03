39f3fadd335671bfca4e66dd47b8add499ff5218
Martin Varga <martin.varga@lutraconsulting.co.uk>
2019-10-03 15:31:02 +0200

    updated pygeodiff version in setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     platforms='any',
     install_requires=[
         'python-dateutil==2.6.0',
-        'pygeodiff==0.3.0'
+        'pygeodiff==0.4.0'
     ],
 
     test_suite='nose.collector',
