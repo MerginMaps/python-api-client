6a984f4d3ef170c1a375687f183d685f44839f20
Peter Petrik <zilolv@gmail.com>
2020-01-30 13:33:36 +0100

    fix #70 ssl for macos

--- mergin/client.py
+++ mergin/client.py
@@ -13,6 +13,7 @@ import copy
 import platform
 from datetime import datetime, timezone
 import concurrent.futures
+import ssl
 
 from pip._vendor import distro
 
@@ -562,7 +563,21 @@ class MerginClient:
                 "username": token_data["username"]
             }
 
-        self.opener = urllib.request.build_opener()
+        # fix for wrong macos installation of python certificates,
+        # see https://github.com/lutraconsulting/qgis-mergin-plugin/issues/70
+        # remove when https://github.com/qgis/QGIS-Mac-Packager/issues/32
+        # is fixed.
+        default_cafile = ssl.get_default_verify_paths().openssl_cafile
+        if os.path.exists(default_cafile):
+            self.opener = urllib.request.build_opener()
+        else:
+            cafile = os.path.join(this_dir, 'cert.pem')
+            if not os.path.exists(default_cafile):
+                raise Exception("missing " + cafile)
+            ctx = ssl.SSLContext()
+            ctx.load_verify_locations(cafile)
+            https_handler = urllib.request.HTTPSHandler(context=ctx)
+            self.opener = urllib.request.build_opener(https_handler)
         urllib.request.install_opener(self.opener)
 
         if login and password:
