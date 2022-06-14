b233b81efa3e4a81f50d915f24dd0bc4fb173133
PeterPetrik <zilolv@gmail.com>
2022-06-14 10:07:53 +0200

    script to bump version

---
+++ scripts/update_version.py
@@ -0,0 +1,29 @@
+import os
+import re
+import argparse
+
+
+def replace_in_file(filepath, regex, sub):
+    with open(filepath, 'r') as f:
+        content = f.read()
+
+    content_new = re.sub(regex, sub, content, flags=re.M)
+
+    with open(filepath, "w") as f:
+        f.write(content_new)
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+parser = argparse.ArgumentParser()
+parser.add_argument('--version', help='version to replace')
+args = parser.parse_args()
+ver = args.version
+print("using version " + ver)
+
+about_file = os.path.join(dir_path, os.pardir, "mergin", "version.py")
+print("patching " + about_file)
+replace_in_file(about_file, "__version__\s=\s'.*", "__version__ = '" + ver + "'")
+
+setup_file = os.path.join(dir_path, os.pardir, "setup.py")
+print("patching " + setup_file)
+replace_in_file(setup_file, "VERSION\s=\s'.*", "VERSION = '" + ver + "'")
