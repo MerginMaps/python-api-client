1d583120a927df1e019fbccd07d8ead0d261921f
Jan Caha <jan.caha@lutraconsulting.co.uk>
2023-06-28 10:20:07 +0200

    add function to clean after push_project

--- mergin/client.py
+++ mergin/client.py
@@ -14,6 +14,7 @@ import dateutil.parser
 import ssl
 from enum import Enum, auto
 import re
+from pathlib import Path
 
 from .common import ClientError, LoginError, InvalidProject
 from .merginproject import MerginProject
@@ -758,6 +759,7 @@ class MerginClient:
             return  # there is nothing to push (or we only deleted some files)
         push_project_wait(job)
         push_project_finalize(job)
+        self.clean_temp_files(directory)
 
     def pull_project(self, directory):
         """
@@ -990,3 +992,15 @@ class MerginClient:
         """
         info = self.project_info(project_path)
         return info["permissions"]["upload"]
+
+    def clean_temp_files(self, directory: str) -> None:
+        """
+        Removes all files matchning patter "*-diff-*" from .mergin folder inside specified folder.
+
+        :param directory: project's local path
+        :type directory: str
+        """
+        mergin_dir = Path(directory) / ".mergin"
+        if mergin_dir.exists():
+            for file in mergin_dir.glob("*-diff-*"):
+                file.unlink()
