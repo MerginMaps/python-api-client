5e8c8a5a8b9d757a5d06e61195f0dc8026e6129f
marcel.kocisek <marcel.kocisek@lutraconsulting.co.uk>
2024-05-31 11:15:43 +0200

    Cleanup edtor CLI tests

--- scripts/editor_cli/config.sh
+++
@@ -1,7 +0,0 @@
-export MERGIN_CLI_PATH=../python-api-client/mergin/cli.py
-export MERGIN_URL=http://127.0.0.1:5004
-export PROJECT=juju/python-client-test
-export EDITOR_USERNAME=merginuser2
-export EDITOR_PASSWORD=Top_secret
-export MERGIN_USERNAME=admin
-export MERGIN_PASSWORD=Top_secret
