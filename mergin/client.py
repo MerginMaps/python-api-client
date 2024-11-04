23fa36671aa3a50ec13614d655ecd9a4e0424bf9
marcel.kocisek <marcel.kocisek@lutraconsulting.co.uk>
2024-11-04 13:43:36 +0100

    Black

--- mergin/client.py
+++ mergin/client.py
@@ -236,7 +236,7 @@ class MerginClient:
             data = json.dumps(data, cls=DateTimeEncoder).encode("utf-8")
         request = urllib.request.Request(url, data, headers, method="POST")
         return self._do_request(request)
-    
+
     def patch(self, path, data=None, headers={}):
         url = urllib.parse.urljoin(self.url, urllib.parse.quote(path))
         if headers.get("Content-Type", None) == "application/json":
