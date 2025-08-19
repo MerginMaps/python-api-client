423f76c4d7233d700fceb442ac801042092bbc06
marcel.kocisek <marcel.kocisek@lutraconsulting.co.uk>
2025-08-19 16:03:51 +0200

    Bump 0.10.4

--- mergin/version.py
+++ mergin/version.py
@@ -1,5 +1,5 @@
 # The version is also stored in ../setup.py
-__version__ = "0.10.3"
+__version__ = "0.10.4"
 
 # There seems to be no single nice way to keep version info just in one place:
 # https://packaging.python.org/guides/single-sourcing-package-version/
