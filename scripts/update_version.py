59d3ccc6214fcc7f3d0a194c3a75ef99ceb27489
Herman Snevajs <herman.snevajs@lutraconsulting.co.uk>
2025-09-11 21:52:21 +0200

    Revert "black"
    This reverts commit 97dba51712efb267c04f1aab904fa6f2c8827add.

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -4,7 +4,7 @@ import argparse
 
 
 def replace_in_file(filepath, regex, sub):
-    with open(filepath, "r") as f:
+    with open(filepath, 'r') as f:
         content = f.read()
 
     content_new = re.sub(regex, sub, content, flags=re.M)
@@ -15,14 +15,14 @@ def replace_in_file(filepath, regex, sub):
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 parser = argparse.ArgumentParser()
-parser.add_argument("--version", help="version to replace")
+parser.add_argument('--version', help='version to replace')
 args = parser.parse_args()
 ver = args.version
 print("using version " + ver)
 
 about_file = os.path.join(dir_path, os.pardir, "mergin", "version.py")
 print("patching " + about_file)
-replace_in_file(about_file, '__version__\s=\s".*', '__version__ = "' + ver + '"')
+replace_in_file(about_file, "__version__\s=\s\".*", "__version__ = \"" + ver + "\"")
 
 setup_file = os.path.join(dir_path, os.pardir, "setup.py")
 print("patching " + setup_file)
--- setup.py
+++ setup.py
@@ -4,32 +4,36 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mergin-client",
-    version="0.10.5",
-    url="https://github.com/MerginMaps/python-api-client",
-    license="MIT",
-    author="Lutra Consulting Ltd.",
-    author_email="info@merginmaps.com",
-    description="Mergin Maps utils and client",
-    long_description="Mergin Maps utils and client",
+    name='mergin-client',
+    version='0.10.5',
+    url='https://github.com/MerginMaps/python-api-client',
+    license='MIT',
+    author='Lutra Consulting Ltd.',
+    author_email='info@merginmaps.com',
+    description='Mergin Maps utils and client',
+    long_description='Mergin Maps utils and client',
+
     packages=find_packages(),
-    platforms="any",
+
+    platforms='any',
     install_requires=[
-        "python-dateutil==2.8.2",
-        "pygeodiff==2.0.4",
-        "pytz==2022.1",
-        "click==8.1.3",
-        "pathvalidate==3.3.1",
+        'python-dateutil==2.8.2',
+        'pygeodiff==2.0.4',
+        'pytz==2022.1',
+        'click==8.1.3',
+        'pathvalidate==3.3.1',
     ],
+
     entry_points={
-        "console_scripts": ["mergin=mergin.cli:cli"],
+        'console_scripts': ['mergin=mergin.cli:cli'],
     },
+
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3'
     ],
-    package_data={"mergin": ["cert.pem"]},
+    package_data={'mergin': ['cert.pem']}
 )
