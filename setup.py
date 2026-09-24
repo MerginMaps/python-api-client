8d3969aac203b07d0da219561481c7cdf131e446
Herman Snevajs <herman.snevajs@lutraconsulting.co.uk>
2026-09-24 08:36:51 +0200

    bump py-client version to 0.15.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mergin-client",
-    version="0.14.3",
+    version="0.15.0",
     url="https://github.com/MerginMaps/python-api-client",
     license="MIT",
     author="Lutra Consulting Ltd.",
